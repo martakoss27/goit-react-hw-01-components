f8f67e1804927792dea60b6149d7eb9b000c762a
MartaKoss27 <27691497m@gmail.com>
2023-10-10 14:46:24 +0200

    Pt.3 Friends component created

---
+++ src/components/Friends/Friends.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+export const Friends = ({ friends }) => (
+  <ul class="friend-list">
+    {friends.map(({ id, name, avatar, isOnline }) => (
+      <FriendsItem key={id} avatar={avatar} name={name} isOnline={isOnline} />
+    ))}
+  </ul>
+);
+//
+Friends.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
+const FriendsItem = ({ id, name, avatar, isOnline }) => (
+  <li class="item" key={id}>
+    <span class="status">{isOnline}</span>
+    <img class="avatar" src={avatar} alt="User avatar" width="48" />
+    <p class="name">{name}</p>
+  </li>
+);
